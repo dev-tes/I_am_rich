b957f0ea0ef7d86fda6ec5274a281045303f352f
dev-tes <tesleem.amuda@gmail.com>
2021-11-03 08:43:14 +0100

    update title

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,7 @@ void main() {
     home: Scaffold(
       backgroundColor: Colors.blueGrey,
       appBar: AppBar(
-        title: Text('I Am Rich'),
+        title: Text('I Am Indeed Rich'),
         backgroundColor: Colors.blueGrey[900],
       ),
       body: Center(
